bb99eb723706ef4fecac8f51a7b5c9ef527e9113
Piyush Kumar Singh <142776115+strawhat-90@users.noreply.github.com>
2024-01-08 16:00:52 +0530

    Add files via upload

---
+++ constructer_distructer.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Employee
+{
+    private:
+    int salary;
+
+    public:
+    // Employee()
+    // {
+    //     cout<<"Default constructer is called...";
+    //     salary=50000;
+    //     cout<<salary;
+    // }
+
+};
+
+
+
+int main()
+{
+    cout<<E1.salary;
+ return 0; 
+}
